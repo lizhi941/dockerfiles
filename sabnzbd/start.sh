27726d2d1c3472eaaf6b9866c852e3351a2978d5
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2014-03-01 23:02:26 +1000

    fixed sabnzbd start script

--- sabnzbd/start.sh
+++ sabnzbd/start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-/usr/bin/sabnzbdplus --daemon --config-file /config --server :8080
+/usr/bin/sabnzbd --daemon --config-file /config --server :8080
 sleep 5
 
 tail -f /config/logs/sabnzbd.*
