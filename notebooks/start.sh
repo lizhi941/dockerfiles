dc660876eb57660bcb1215c7a47d527af63c7565
Taylor "Nekroze" Lawson <taylorjalawson@gmail.com>
2014-08-02 19:47:27 +1000

    Correct the profile location

--- notebooks/start.sh
+++ notebooks/start.sh
@@ -1,4 +1,4 @@
-cat <<EOF >> /root/.ipython/profile_default/ipython_notebook_config.py
+cat <<EOF >> /.ipython/profile_default/ipython_notebook_config.py
 c.IPKernelApp.pylab = 'inline'
 c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
@@ -9,7 +9,7 @@ EOF
 if [ ! -z "$PASSWORD" ]; then
   # use password on notebook
   HASH=$(python -c "from IPython.lib import passwd; print(passwd('${PASSWORD}'))" )
-  echo "c.NotebookApp.password =u'${HASH}'" >> /root/.ipython/profile_default/ipython_notebook_config.py
+  echo "c.NotebookApp.password =u'${HASH}'" >> /.ipython/profile_default/ipython_notebook_config.py
   unset PASSWORD
 fi
 
