18ac05d2a9990dae4f61c14b62d4d5ae5f99fb42
Taylor "Nekroze" Lawson <taylorjalawson@gmail.com>
2014-08-03 10:21:50 +1000

    Added certificate generation

--- notebooks/start.sh
+++ notebooks/start.sh
@@ -1,3 +1,13 @@
+# Strict mode
+set -euo pipefail
+IFS=$'\n\t' 
+
+# Create a self signed certificate for the user if one doesn't exist
+if [ ! -f $PEM_FILE ]; then
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $PEM_FILE -out $PEM_FILE \
+    -subj "/C=XX/ST=XX/L=XX/O=dockergenerated/CN=dockergenerated"
+fi
+
 if [ ! -z "$PASSWORD" ]; then
   # use password on notebook
   HASH=$(python -c "from IPython.lib import passwd; print(passwd('${PASSWORD}'))" )
@@ -5,4 +15,4 @@ if [ ! -z "$PASSWORD" ]; then
   unset PASSWORD
 fi
 
-ipython notebook
+ipython notebook --certfile=$PEM_FILE
