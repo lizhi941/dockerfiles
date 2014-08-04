c24d5ad279f47db24bf63a468f5c9dabf549b619
Taylor "Nekroze" Lawson <taylorjalawson@gmail.com>
2014-08-04 11:31:23 +1000

    Removed certification file

--- notebooks/start.sh
+++ notebooks/start.sh
@@ -2,12 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t' 
 
-# Create a self signed certificate for the user if one doesn't exist
-if [ ! -f $PEM_FILE ]; then
-  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $PEM_FILE -out $PEM_FILE \
-    -subj "/C=XX/ST=XX/L=XX/O=dockergenerated/CN=dockergenerated"
-fi
-
 if [ ! -z "$PASSWORD" ]; then
   # use password on notebook
   HASH=$(python -c "from IPython.lib import passwd; print(passwd('${PASSWORD}'))" )
@@ -15,4 +9,4 @@ if [ ! -z "$PASSWORD" ]; then
   unset PASSWORD
 fi
 
-ipython notebook --certfile=$PEM_FILE
+ipython notebook
