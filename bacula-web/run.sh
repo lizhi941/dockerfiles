f6427b172a4aea87c570adf83a54dc412fce2ca5
Taylor Nekroze Lawson <tlawson@nekroze.com>
2014-11-20 14:25:38 +1000

    corrected config index

--- bacula-web/run.sh
+++ bacula-web/run.sh
@@ -5,13 +5,13 @@ cat > /var/www/html/application/config/config.php <<EOF
 \$config[0]['language'] = 'en_US';
 \$config['show_inactive_clients'] = false;
 \$config['hide_empty_pools'] = true;
-\$config[1]['label'] = 'Database';
-\$config[1]['host'] = '$DB_HOST';
-\$config[1]['login'] = '$DB_USER';
-\$config[1]['password'] = '$DB_PASS';
-\$config[1]['db_name'] = '$DB_NAME';
-\$config[1]['db_type'] = '$DB_TYPE';
-\$config[1]['db_port'] = '$DB_PORT';
+\$config[0]['label'] = 'Database';
+\$config[0]['host'] = '$DB_HOST';
+\$config[0]['login'] = '$DB_USER';
+\$config[0]['password'] = '$DB_PASS';
+\$config[0]['db_name'] = '$DB_NAME';
+\$config[0]['db_type'] = '$DB_TYPE';
+\$config[0]['db_port'] = '$DB_PORT';
 ?>
 EOF
 
