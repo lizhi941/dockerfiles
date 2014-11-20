13c1444eec5e7cbd456eb6406fa035d7566f3a80
Taylor Nekroze Lawson <tlawson@nekroze.com>
2014-11-20 14:13:19 +1000

    escaped the $ in config lines

--- bacula-web/run.sh
+++ bacula-web/run.sh
@@ -2,16 +2,16 @@
 
 cat > /var/www/html/application/config/config.php <<EOF
 <?php
-$config[0]['language'] = 'en_EN';
-$config['show_inactive_clients'] = false;
-$config['hide_empty_pools'] = true;
-$config[1]['label'] = 'Database';
-$config[1]['host'] = '$DB_HOST';
-$config[1]['login'] = '$DB_USER';
-$config[1]['password'] = '$DB_PASS';
-$config[1]['db_name'] = '$DB_NAME';
-$config[1]['db_type'] = '$DB_TYPE';
-$config[1]['db_port'] = '$DB_PORT';
+\$config[0]['language'] = 'en_EN';
+\$config['show_inactive_clients'] = false;
+\$config['hide_empty_pools'] = true;
+\$config[1]['label'] = 'Database';
+\$config[1]['host'] = '$DB_HOST';
+\$config[1]['login'] = '$DB_USER';
+\$config[1]['password'] = '$DB_PASS';
+\$config[1]['db_name'] = '$DB_NAME';
+\$config[1]['db_type'] = '$DB_TYPE';
+\$config[1]['db_port'] = '$DB_PORT';
 ?>
 EOF
 
