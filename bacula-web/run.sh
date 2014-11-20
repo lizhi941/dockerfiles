07a4329b45f9bf5823d082b1dcdcef328b74b7d0
Taylor Nekroze Lawson <tlawson@nekroze.com>
2014-11-20 13:45:15 +1000

    moved to url root

--- bacula-web/run.sh
+++ bacula-web/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cat > /var/www/html/bacula-web/application/config/config.php <<EOF
+cat > /var/www/html/application/config/config.php <<EOF
 <?php
 $config[0]['language'] = 'en_EN';
 $config['show_inactive_clients'] = false;
@@ -16,11 +16,5 @@ $config[1]['db_port'] = '$DB_PORT';
 EOF
 
 echo "date.timezone = $PHP_TIMEZONE" >> /etc/php.ini
-
-cat >> /etc/httpd/conf.d/bacula-web.conf <<EOF
-<Directory /var/www/html/bacula-web>
-    AllowOverride All
-</Directory>
-EOF
 
 /usr/sbin/httpd -D FOREGROUND
